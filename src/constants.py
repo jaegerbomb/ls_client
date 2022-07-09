f0205cc15097685a694090f19b4106cd27e48c99
Jaeger Bomb <jaegerbomb23@gmail.com>
2022-07-09 11:16:08 -0400

    - added aws public dns api

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,2 @@
 api_url = "http://localhost:8080/api"
+api_url_aws = "http://ec2-35-175-133-100.compute-1.amazonaws.com:8888/api"
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from LoanStreet import LoanStreet
-from constants import api_url
+from constants import api_url, api_url_aws
 
 if __name__ == "__main__":
-    street = LoanStreet(url=api_url)
+    street = LoanStreet(url=api_url_aws)
     print(street.check_health())
     loan_id = f'{street.add_loan(500000, 3.32323, 30, 40)}'
     # add a few more loans
